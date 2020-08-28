6676f0065e6777124ffb759cea731ad400a11057
Arafat1999 <arafathonpode@gmail.com>
2020-08-29 03:33:09 +0530

    Store the Daily Wage along with the Total Wage

---
+++ EmpWageComputationeight.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+WagePerHour=20
+HourCond=100
+Month=20
+day=0
+hour=0
+
+function workHour()
+{
+   case $1 in
+         1)
+            workingHour=8
+            ;;
+         2)
+            workingHour=4
+            ;;
+         *)
+            workingHour=0
+            ;;
+      esac
+      echo $workingHour
+}
+function dailyWage() {
+   local timeHour=$1
+   dailyIncome=$(($timeHour*$WagePerHour))
+   echo $dailyIncome
+}
+while(( day<Month && hour<HourCond ))
+do
+   randomCheck=$((RANDOM%3))
+   emphour=$(workHour $randomCheck)
+   hour=$(($hour+$emphour))
+   wageArray[day]=$(dailyWage $emphour)
+   ((day++))
+done
+
+salary=$(($WagePerHour*$hour))
+   echo "Total Salary Is:" $salary
+   echo "Daily income Is:" ${wageArray[@]}
+
+
